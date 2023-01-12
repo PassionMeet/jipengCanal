31ddcb0441773752098d2d3941431843b41c62be
mingchen@ahaent.com <mingchen@ahaent.com>
2023-01-12 13:20:19 +0800

    maint:测试可用性，不可用，考虑使用java 的canal

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"github.com/go-mysql-org/go-mysql/canal"
+	"github.com/siddontang/go-log/log"
+)
+
+type MyEventHandler struct {
+	canal.DummyEventHandler
+}
+
+func (h *MyEventHandler) OnRow(e *canal.RowsEvent) error {
+	log.Infof("%s %v\n", e.Action, e.Rows)
+	return nil
+}
+
+func (h *MyEventHandler) String() string {
+	return "MyEventHandler"
+}
+
+func main() {
+	cfg := canal.NewDefaultConfig()
+	cfg.Addr = "127.0.0.1:3306"
+	cfg.User = "root"
+	cfg.Password = "123456"
+	cfg.ParseTime = true
+	cfg.Flavor = "mysql"
+	// We only care table canal_test in test db
+	cfg.Dump.TableDB = "jipeng"
+	cfg.Dump.Tables = []string{"users"}
+
+	c, err := canal.NewCanal(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Register a handler to handle RowsEvent
+	c.SetEventHandler(&MyEventHandler{})
+
+	// Start canal
+	c.Run()
+}
